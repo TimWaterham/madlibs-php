8a4c5d588fea7502ad0180e635beff2baf93be2d
Tim Waterham <99021474@mydavinci.nl>
2017-04-03 13:58:58 +0200

    Finished Madlibs in one go, R6S Story

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Madlib story</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<main>
+		<section>
+		<h1>Welcome to the madlib story board</h1>
+		<h2>fill in all the inputs and click "press it" </h2>
+	
+			<form action='madlibget.php' method="get">
+			<p>Whats Your Favourite Rainbow Six Siege Operator?</p?>
+			<p>Operator<input id="i1" type="text" name="operator"></p>
+			<p>Name the Abillity of the operator<input id="i3" type="text" name="abillity"></p>
+			<p>Name your favourite weapon<input id="i4" type="text" name="weapon"></p>
+			<p>Name the secondary weapon<input id="i5" type="text" name="secondaryweapon"></p>
+			<p>Name the Gadget you use with this operator<input id="i6" type="text" name="gadget"></p>
+			<p>Why this Operator<input id="i6" type="text" name="why"></p>
+			<p><input type="submit" value="Press it"></p>
+			</form>
+		</section>
+	</main>
+</body>
+</html>
---
+++ madlibget.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Madlib story</title>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css">
+</head>
+<body>
+	<main>
+	<section id="section2">
+	<p>Youve chosen Operator <?php echo $_GET["operator"]; ?>, </p>
+
+	<p>Youve been deployed to secure a objective in the middle of this compound, toward the objective, after a while you find out that your <?php echo $_GET["abillity"]; ?> no longer functions, so  all you got left is your <?php echo $_GET["weapon"]; ?> Only a few rounds left in your primary, but atleast you have your secondary <?php echo $_GET["secondaryweapon"]; ?>, you can only use your <?php echo $_GET["gadget"]; ?> to breach, or melee through..... all it leaves is your answer why youve chosen this operator, you answered : <?php echo $_GET["why"]; ?> but still why youve chosen this operator with this gadget ill never know.... but the narrator wants to advise you to bring more ammo and check your gadget next time.
+		</p> 
+	</section>
+	</main>
+</body>
+</html>
